e7a9ee88116a968c47f00cdfb76c344fc7c26371
ekkifirdiansyah <73180261+ekkifirdiansyah@users.noreply.github.com>
2024-06-25 17:02:12 +0700

    initialize

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -5,20 +5,75 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
-    );
+        appBar: AppBar(
+            centerTitle: false,
+            title: RichText(
+              text: TextSpan(
+                text: "Hai, ",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+                children: [
+                  TextSpan(
+                    text: "Muhammad",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            actions: [
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  margin: EdgeInsets.only(right: 20),
+                  width: 30,
+                  height: 30,
+                  child: Image.asset(
+                    "assets/icons/qrcode.png",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ]),
+        body: Stack(
+          children: [
+            ClipPath(
+              clipper: ClipPathClass(),
+              child: Container(
+                height: 250,
+                width: Get.width,
+                color: Color(0xFFEC2020),
+              ),
+            ),
+          ],
+        ));
   }
+}
+
+class ClipPathClass extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height - 60);
+    path.quadraticBezierTo(
+      size.width / 2,
+      size.height,
+      size.width,
+      size.height - 60,
+    );
+
+    path.lineTo(size.width, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,26 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // return GetMaterialApp(
-    //   debugShowCheckedModeBanner: false,
-    //   title: "Application",
-    //   initialRoute: Routes.LOGIN,
-    //   getPages: AppPages.routes,
-    //
-    return FutureBuilder(
-      future: Future.delayed(Duration(seconds: 3)),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return SplashScreen();
-        } else {
-          return GetMaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: "Application",
-            initialRoute: Routes.LOGIN,
-            getPages: AppPages.routes,
-          );
-        }
-      },
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Application",
+      initialRoute: Routes.HOME,
+      getPages: AppPages.routes,
+    
+    // return FutureBuilder(
+    //   future: Future.delayed(Duration(seconds: 3)),
+    //   builder: (context, snapshot) {
+    //     if (snapshot.connectionState == ConnectionState.waiting) {
+    //       return SplashScreen();
+    //     } else {
+    //       return GetMaterialApp(
+    //         debugShowCheckedModeBanner: false,
+    //         title: "Application",
+    //         initialRoute: Routes.LOGIN,
+    //         getPages: AppPages.routes,
+    //       );
+    //     }
+    //   },
     );
   }
 }
